4b5194aff4ccdbe10639a4f30e48233c862620c4
Aryav Lahiri <aryav.lahiri@clearscore.com>
2025-07-17 14:41:23 +0100

    Minor spelling improvement

--- enhanced_financial_recommender.py
+++ enhanced_financial_recommender.py
@@ -49,7 +49,7 @@ class FinanciallyAwareRecommender:
             # Asset Component (having mortgage/car indicates stability)
             asset_component = (user['has_mortgage'] * 0.6 + user['has_car'] * 0.4)
             
-            # Calculate composite financial health score (0-1) (Weightings can be changed, currently haviest weighting given to credit score maybe ccj should be more)
+            # Calculate composite financial health score (0-1) (Weightings can be changed, currently heaviest weighting given to credit score maybe ccj should be more)
             financial_health = (
                 credit_component * 0.30 +
                 dti_component * 0.25 +
